d3c47053bf06e8aacb882e2a3b15b60825f410da
Daniel194 <lungu.daniel94@gmail.com>
2017-02-25 23:30:30 +0200

    Delete all

--- src/ro/doc/repository/client/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package ro.doc.repository.client;
-
-
-import ro.doc.entities.Client;
-import ro.doc.repository.AppRepository;
-
-public abstract class ClientRepository extends AppRepository<String, Client> {
-
-}
--- src/ro/doc/websocket/handler/WebSocketHandler.java
+++
@@ -1,9 +0,0 @@
-package ro.doc.websocket.handler;
-
-import javax.websocket.CloseReason;
-import javax.websocket.Session;
-
-public interface WebSocketHandler {
-	public void onOpen(Session userSession);
-	public void onClose(CloseReason closeReason, Session userSession);
-}
