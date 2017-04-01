3b4332c1f5eda51276ad81a9483158c62025d525
Daniel194 <lungu.daniel94@gmail.com>
2017-04-01 16:22:51 +0300

    PyModel - Stack

--- src/test/python/pymodel/example/test.py
+++ src/test/python/pymodel/example/test.py
@@ -1,29 +1,14 @@
 """
-PowerSwitch graphics tests
+Test Stack graphics
 """
 
 cases = [
-    ('Generate FSM from PowerSwitch model program',
-     'pma PowerSwitch'),
+    ('Generate FSM',
+     'pma Stack'),
 
-    ('Generate dot graphics commands from generated PowerSwitchFSM',
-     'pmg PowerSwitchFSM'),
+    ('Generate dot commands',
+     'pmg StackFSM'),
 
-    ('Generate SVG file from dot commands',
-     'dotsvg PowerSwitchFSM'),
-
-    ('Generate dot commands from SpeedControl FSM',
-     'pmg SpeedControl'),
-
-    ('Generate SVG file from dot commands',
-     'dotsvg SpeedControl'),
-
-    ('Generate FSM from composition of PowerSwitch and SpeedControl, show interleaving',
-     'pma SpeedControl PowerSwitch -o PowerSpeed'),
-
-    ('Generate dot commands from composed FSM',
-     'pmg PowerSpeed'),
-
-    ('Generate SVG from dot',
-     'dotsvg PowerSpeed')
+    ('Generate SVG file',
+     'dotsvg StackFSM'),
 ]
