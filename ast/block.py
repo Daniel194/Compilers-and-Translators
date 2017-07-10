4edf2c5ce960576fa65e159f6193082c069fcd85
Daniel194 <lungu.daniel94@gmail.com>
2017-07-10 22:54:33 +0300

    New AST

---
+++ ast/block.py
@@ -0,0 +1,7 @@
+from ast.ast import AST
+
+
+class Block(AST):
+    def __init__(self, declarations, compound_statement):
+        self.declarations = declarations
+        self.compound_statement = compound_statement
---
+++ ast/type.py
@@ -0,0 +1,7 @@
+from ast.ast import AST
+
+
+class Type(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
