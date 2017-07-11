407af54d907d3e655b5cf9172718294c463e12b7
Daniel194 <lungu.daniel94@gmail.com>
2017-07-11 23:53:24 +0300

    Symbole table

---
+++ symbole/symbol_table.py
@@ -0,0 +1,30 @@
+from collections import OrderedDict
+from symbole.buldin_type_simbole import BuiltinTypeSymbol
+
+
+class SymbolTable(object):
+    def __init__(self):
+        self._symbols = OrderedDict()
+        self._init_builtins()
+
+    def _init_builtins(self):
+        self.define(BuiltinTypeSymbol('INTEGER'))
+        self.define(BuiltinTypeSymbol('REAL'))
+
+    def __str__(self):
+        s = 'Symbols: {symbols}'.format(
+            symbols=[value for value in self._symbols.values()]
+        )
+        return s
+
+    __repr__ = __str__
+
+    def define(self, symbol):
+        print('Define: %s' % symbol)
+        self._symbols[symbol.name] = symbol
+
+    def lookup(self, name):
+        print('Lookup: %s' % name)
+        symbol = self._symbols.get(name)
+        # 'symbol' is either an instance of the Symbol class or 'None'
+        return symbol
